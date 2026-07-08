743426be26fa260fb6f42895990238f35db4faa5
NikitaNos <nikinos1992@gmail.com>
2026-07-08 16:11:09 +0300

    add serde, and first extractor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::net::TcpListener;
+
 use financial_monitor::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    //Tightly tied to this IP need FIX"
+    let address = "127.0.0.1:8000";
+    let listener = TcpListener::bind(address)?;
+    println!("Server running on {}", address);
+    run(listener)?.await
 }
