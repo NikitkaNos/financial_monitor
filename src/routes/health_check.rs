211adccd7fa34cf1a628118cd79e6ff9af01050a
NikitaNos <nikinos1992@gmail.com>
2026-07-13 16:34:15 +0300

    stable version

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -15,7 +15,7 @@ async fn _spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = run(listener, pool).await.expect("Failed to start server");
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
 #[tokio::test]
