c431cea6463e5434abcf20062b92b3e6c65efb60
NikitaNos <nikinos1992@gmail.com>
2026-08-09 15:42:23 +0300

    First step for tracing_span

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,6 +12,16 @@ pub struct SingupData {
 pub async fn signup(form: web::Json<SingupData>, pool: web::Data<PgPool>) -> HttpResponse {
     let user_id = Uuid::new_v4();
 
+    let request_span = tracing::info_span!(
+        "Adding a new user",
+        %user_id,
+        user_email = %form.email,
+        user_password = %form.password,
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new user details in the DataBase");
+
     let result = sqlx::query!(
         "INSERT INTO users (id, email, password_hash) VALUES($1, $2, $3) RETURNING id",
         user_id,
@@ -18,6 +29,7 @@ pub async fn signup(form: web::Json<SingupData>, pool: web::Data<PgPool>) -> Htt
         form.password,
     )
     .fetch_one(pool.get_ref())
+    .instrument(query_span)
     .await;
 
     match result {
