727b08ab693d5c2f17920a8f7e2a6c2b0b3fdb64
NikitaNos <nikinos1992@gmail.com>
2026-08-12 08:10:07 +0300

    Logs update

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -15,25 +15,25 @@ pub async fn signup(form: web::Json<SingupData>, pool: web::Data<PgPool>) -> Htt
     let user_password = form.password.clone();
 
     let request_span = tracing::info_span!(
-        "ADDING A NEW USER",
+        "ADDING A NEW USER-->",
         user_id = %user_id,
         user_email = %user_email,
         user_password = %user_password,
     );
     let _request_span_guard = request_span.enter();
 
-    tracing::info!("[ADDING A NEW USER - START]");
+    tracing::info!("[ADDING A NEW USER - START]-->");
 
     // Для запроса используем отдельный Span без enter()
     let query_span = tracing::info_span!(
-        "SAVING NEW USER DETAILS IN THE DATABASE",
+        "SAVING NEW USER DETAILS IN THE DATABASE-->",
         user_id = %user_id,
         user_email = %user_email,
         user_password = %user_password,
     );
 
     // Логируем старт внутри Span через событие
-    tracing::info!("[SAVING NEW USER DETAILS IN THE DATABASE - START]");
+    tracing::info!("[SAVING NEW USER DETAILS IN THE DATABASE - START]-->");
 
     match sqlx::query!(
         "INSERT INTO users (id, email, password_hash) VALUES($1, $2, $3) RETURNING id",
@@ -46,8 +46,8 @@ pub async fn signup(form: web::Json<SingupData>, pool: web::Data<PgPool>) -> Htt
     .await
     {
         Ok(_) => {
-            tracing::info!("[SAVING NEW USER DETAILS IN THE DATABASE - END]");
-            tracing::info!("[ADDING A NEW USER - EVENT] User created successfully");
+            tracing::info!("[<--SAVING NEW USER DETAILS IN THE DATABASE - END]");
+            tracing::info!("[<--ADDING A NEW USER - EVENT] User created successfully");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
