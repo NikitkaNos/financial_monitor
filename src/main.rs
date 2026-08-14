2da8192cad3858b2294a8993c4993cb34115b20c
NikitaNos <nikinos1992@gmail.com>
2026-08-14 10:16:13 +0300

    Big refactoring main

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 use financial_monitor::configuration::get_configuration;
 use financial_monitor::run;
+use financial_monitor::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::{fs::File, net::TcpListener};
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{EnvFilter, Registry, fmt, layer::SubscriberExt};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // 1. Настраиваем логирование
     let log_file = File::create("logs.json").expect("Failed to create log file");
-    let formatting_layer = BunyanFormattingLayer::new("financial_monitor".into(), log_file);
-    let console_layer = fmt::layer().pretty();
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-        .with(console_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("financial_monitor".into(), "info".into(), Some(log_file));
+    init_subscriber(subscriber);
 
+    // 2. Загружаем .env
     dotenvy::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL не найден");
     tracing::info!("Database URL: {}", database_url);
 
+    // 3. Загружаем конфигурацию и подключаемся к БД
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
     let pool = PgPool::connect(&connection_string)
         .await
         .expect("Failed to connect to DB");
 
+    // 4. Запускаем сервер
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     let server = run(listener, pool).await?;
     server.await?;
